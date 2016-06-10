71ebd1639b6070c0aed20d025a8f58710131b0de
Matias Aravena <aravenagamboa@gmail.com>
2016-06-10 11:59:16 -0300

    Test Git (solving conflict).

--- SophiaGUI/explora/views.py
+++ SophiaGUI/explora/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def index(request):
-    return render(request,'explora/home.html')
+     return HttpResponse("Hello world. You're at the Sophia GUI index !!!!!. And we are testing GitHub.")
