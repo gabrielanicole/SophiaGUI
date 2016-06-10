0bca284a5f4bec6fd87bc8b67c57427d43abe0f9
Matias Aravena <aravenagamboa@gmail.com>
2016-06-10 12:06:36 -0300

    Test Git.

--- SophiaGUI/explora/views.py
+++ SophiaGUI/explora/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-     return HttpResponse("Hello world. You're at the Sophia GUI index !!!!!. And we are testing GitHub.")
+         return render(request,'explora/home.html')
