53523ec1e9aefc3efdda15fa9b16a28a0cdca132
Matias Aravena <aravenagamboa@gmail.com>
2016-06-14 18:19:30 -0300

    Test of home page

--- SophiaGUI/explora/views.py
+++ SophiaGUI/explora/views.py
@@ -1,5 +1,8 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
 
 def index(request):
-         return render(request,'explora/home.html')
+    return render(request,'explora/home.html')
+
+def login(request):
+    return render(request,'explora/login.html')
