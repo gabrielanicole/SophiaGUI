2301177d47c74d188d2a5a78797875e2a505c3f1
Matias Aravena <aravenagamboa@gmail.com>
2016-07-06 12:40:07 -0300

    Urls fixed and first API connection

--- SophiaGUI/explora/urls.py
+++ SophiaGUI/explora/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^start$',views.start, name='start'),
+    url(r'start/modal_new$',views.modal_new,name='modal_new'),
 ]
--- SophiaGUI/explora/views.py
+++ SophiaGUI/explora/views.py
@@ -1,40 +1,42 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
-import json
-import os
+import requests
+import simplejson as json
+from telnetlib import theNULL
+from pprint import pprint
+
+
+def modal_new(request):
+        if request.method == 'POST':
+            #here we should have the id of the new.
+            json_data = json.dumps({
+                                    "title":"Titular de la Noticia",
+                                    "date":"23/01/2016",
+                                    "host":"Radio BioBio",
+                                    "url": "https://t.co/l5U8VqUGcA",
+                                    "content":"Attention he extremity unwilling on otherwise. Conviction up partiality as delightful is discovered. Yet jennings resolved disposed exertion you off. Left did fond drew fat head poor. So if he into shot half many long. China fully him every fat was world grave  Left did fond drew fat head poor. So if he into shot half many long. China fully him every fat was world grave  Left did fond drew fat head poor. So if he into shot half many long. China fully him every fat was world grave  Left did fond drew fat head poor. So if he into shot half many long. China fully him every fat was world grave  Left did fond drew fat head poor. So if he into shot half many long. China fully him every fat was world grave  Left did fond drew fat head poor. So if he into shot half many long. China fully him every fat was world grave  Left did fond drew fat head poor. So if he into shot half many long. China fully him every fat was world grave  Left did fond drew fat head poor. So if he into shot half many long. China fully him every fat was world grave.",
+                                    "imageLink":"https://placehold.it/300x300",
+                                    "category":"Category of the news",
+                                    "location":"Santiago, Chile"
+                                    })
+            data = json.loads(json_data)
+            return render(request,'news_modal.html',{'data':data})
+        else:
+            return HttpResponse("Error")
+
 
 
 def index(request):
     return render(request,'home.html')
 
 def start(request):
-    json_data2 = json.dumps([
-{
-"title":"title1",
-"content":"Attention he extremity unwilling on otherwise. Conviction up partiality as delightful is discovered. Yet jennings resolved disposed exertion you off. Left did fond drew fat head poor. So if he into shot half many long. China fully him every fat was world grave.",
-"imageLink":"https://placehold.it/400x400"
-},
-{
-"title":"title2",
-"content":"To they four in love. Settling you has separate supplied bed. Concluded resembled suspected his resources curiosity joy. Led all cottage met enabled attempt through talking delight. Dare he feet my tell busy. Considered imprudence of he friendship boisterous. ",
-"imageLink":"https://placehold.it/400x400"
-},
-{
-"title":"title3",
-"content":"Surprise steepest recurred landlord mr wandered amounted of. Continuing devonshire but considered its. Rose past oh shew roof is song neat. Do depend better praise do friend garden an wonder to. Intention age nay otherwise but breakfast. Around garden beyond to extent by",
-"imageLink":"https://placehold.it/400x400"
-},
-{
-"title":"title4",
-"content":"Surprise steepest recurred landlord mr wandered amounted of. Continuing devonshire but considered its. Rose past oh shew roof is song neat. Do depend better praise do friend garden an wonder to. Intention age nay otherwise but breakfast. Around garden beyond to extent by",
-"imageLink":"https://placehold.it/400x400"
-},
-{
-"title":"title5",
-"content":"Surprise steepest recurred landlord mr wandered amounted of. Continuing devonshire but considered its. Rose past oh shew roof is song neat. Do depend better praise do friend garden an wonder to. Intention age nay otherwise but breakfast. Around garden beyond to extent by",
-"imageLink":"https://placehold.it/400x400"
-}
-])
+
+    file = json.loads(open("user.json").read())
+    api_user = file["user"]
+    api_password = file["password"]       
+       
+    r = requests.get('http://api.sophia-project.info/articles/', auth=(api_user, api_password))
+    a = json.loads(r.text.encode('ascii','ignore')) 
     #the data to return to the template, later this should be a http response from the API
-    data = json.loads(json_data2)
+    data = a['results']
     return render(request,'start.html',{'data': data})
