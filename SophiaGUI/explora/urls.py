6cf30abd2831692310b85fcdefa7761d791c2826
Matias Aravena <aravenagamboa@gmail.com>
2016-07-18 11:48:41 -0400

    fixed urls

--- SophiaGUI/explora/urls.py
+++ SophiaGUI/explora/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     url(r'articles/modal_new$',views.modal_new,name='modal_new'),
     url(r'^logout/$',views.logout, name='logout'),
     url(r'^login$',views.login, name='login'),
-    url(r'not_logged$',views.user_not_logged,name='user_not_logged'),
+    url(r'login_required$',views.user_not_logged,name='user_not_logged'),
 ]
--- SophiaGUI/explora/views.py
+++ SophiaGUI/explora/views.py
@@ -23,17 +23,17 @@ def login(request):
         #here should be an else for when user account is disbled
         else:
             return redirect('index')
-        
+
 def modal_new(request):
         if request.method == 'POST':
             #here we should have the id of the new.
             news_pk = request.POST['news_id']
-            
+
             file = json.loads(open("explora/static/user.json").read())
             api_user = file["user"]
             api_password = file["password"]
-            
-            api_url = u'http://api.sophia-project.info/articles/{0}/'.format(news_pk) 
+
+            api_url = u'http://api.sophia-project.info/articles/{0}/'.format(news_pk)
             r = requests.get(api_url, auth=(api_user, api_password))
             a = json.loads(r.text.encode('utf8'))
             #Here come the data
@@ -43,7 +43,7 @@ def modal_new(request):
             url = a['url']
             content = a['content']
             imageLink = a['imageLink']
-                    
+
             return render(request,'news_modal.html',{'title':title,
                                                      'date':date,
                                                      'host':host,
@@ -51,53 +51,53 @@ def modal_new(request):
                                                      'content':content,
                                                      'imageLink':imageLink})
         else:
-            return HttpResponse("Error")
+            return HttpResponse("Internal Error")
 
 
 def user_not_logged(request):
     if request.user.is_authenticated():
         return redirect('articles')
     else:
-        return render(request,'content_not_available.html')
+        return render(request,'login_required.html')
 
 def index(request):
-       
+
     if request.user.is_authenticated():
         return redirect('articles')
     else:
         return render(request,'home.html')
 
 
-#here we should redirect 
-@login_required(login_url='/not_logged')
+#here we should redirect
+@login_required(login_url='/login_required')
 def articles(request, num="1"):
-           
+
     pages = 10
     num_pages = []
     for i in range(1,pages):
         num_pages.append(i)
-    
+
     file = json.loads(open("explora/static/user.json").read())
     api_user = file["user"]
-    api_password = file["password"]       
-    
-    api_url = u'http://api.sophia-project.info/articles/?page={0}'.format(num)   
+    api_password = file["password"]
+
+    api_url = u'http://api.sophia-project.info/articles/?page={0}'.format(num)
     r = requests.get(api_url, auth=(api_user, api_password))
     a = json.loads(r.text.encode('utf8'))
     #the data to return to the template, later this should be a http response from the API
     data = a['results']
-    
+
     #Search if is a social user
     my_user = request.user.social_auth.filter(provider='facebook').first()
     if my_user:
-        #if is someone from facebook we get the profile image  
+        #if is someone from facebook we get the profile image
         url = u'https://graph.facebook.com/{0}/picture'.format(my_user.uid)
         return render(request,'articles.html',{'data': data
                                                ,'user':request.user.get_full_name()
                                                ,'profile_pic':url
                                                ,'num_pages':num_pages
                                                })
-    else:      
+    else:
         return render(request,'articles.html',{'data': data
                                                ,'user':request.user.get_full_name()
                                                ,'num_pages':num_pages
