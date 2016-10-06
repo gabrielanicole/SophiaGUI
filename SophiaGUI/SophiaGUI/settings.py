0825b85c44cd44140fed9d7cbe2a43ea3fd9ec2d
maravenag <aravenagamboa@gmail.com>
2016-10-06 15:08:08 -0300

    'fixed settings.py'

--- SophiaGUI/SophiaGUI/settings.py
+++ SophiaGUI/SophiaGUI/settings.py
@@ -148,11 +148,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+#STATIC_URL = '/static/'
+
+#STATICFILES_DIRS = (
+#                    os.path.join(BASE_DIR,'static'),
+#                    )
+
+#PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+#STATIC_ROOT = os.path.join(PROJECT_ROOT, '/explora/static')
+
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-                    os.path.join(BASE_DIR,'static'),
-                    )
+STATIC_ROOT = os.path.join(PROJECT_DIR, '../explora/static/')
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, '/explora/static')
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_DIR=os.path.dirname(__file__)
