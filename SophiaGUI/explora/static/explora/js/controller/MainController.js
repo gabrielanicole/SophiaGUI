4e8b44b77d777324e82fbf002af03bbafd073f02
maravenag <aravenagamboa@gmail.com>
2017-01-06 11:54:08 -0300

    fixed pagination bug

--- SophiaGUI/explora/static/explora/js/controller/MainController.js
+++ SophiaGUI/explora/static/explora/js/controller/MainController.js
@@ -1,96 +1,163 @@
 
-app.controller('searchController', ['$scope','$http','$window', function($scope, $http, $window) {
-   
-   $scope.index = 5;
-   $scope.size = 4;
-   $scope.total_pages;
-   $scope.articulos;
+app.controller('searchController', ['$scope', '$http', '$window', function ($scope, $http, $window) {
 
-   
-   $scope.page_number = function(page_number){
-       //Function to get the page number from view
-       var page = page_number[0][0];
-       if(page == $scope.index - $scope.size && page != 1){
+    $scope.index = 5;
+    $scope.size = 4;
+    $scope.total_pages;
+    $scope.articulos;
+
+
+    $scope.page_number = function (page_number) {
+        //Function to get the page number from view
+        var page = page_number[0][0]-1;
+        if (page == $scope.index - $scope.size && page != 1) {
             $scope.index = $scope.index - 1;
             $scope.update_list(page);
-            
-       }
-       else if(page == $scope.index + $scope.size && $scope.index + $scope.size != $scope.total_pages){
-           $scope.index = $scope.index + 1;
-           $scope.update_list(page);
-       }
-       else{
-           $scope.index = $scope.index;
-           $scope.update_list(page);
-       }
-   }
 
-   $scope.update_list = function(page){
+        }
+        else if (page == $scope.index + $scope.size && $scope.index + $scope.size != $scope.total_pages) {
+            $scope.index = $scope.index + 1;
+            $scope.update_list(page);
+        }
+        else {
+            $scope.index = $scope.index;
+            $scope.update_list(page);
+        }
+    }
+
+    $scope.update_list = function (page) {
         $http({
-                method: 'POST',
-                url: '/get_data/articles/'+page+'/'
-                }).then(function successCallback(response) {
-                    $scope.articulos = response.data.results;
-                    $scope.total_pages = response.data.totalpages;
+            method: 'POST',
+            url: '/get_data/articles/' + page + '/'
+        }).then(function successCallback(response) {
+            $scope.articulos = response.data.results;
+            $scope.total_pages = response.data.totalpages;
 
-                }, function errorCallback(response) {
+        }, function errorCallback(response) {
 
-                });
-   }
-   //load the first page
-   $scope.update_list(1);
-   
-   $scope.range = function(min, max){
+        });
+    }
+    //load the first page
+    $scope.update_list(0);
+
+    $scope.range = function (min, max) {
         var output = [];
-        for(var i = min; i <= max; i++){
-            output.push(i); 
+        for (var i = min; i <= max; i++) {
+            output.push(i);
         }
         return output;
     }
 
- var granularity = 'day';
-    
-    $scope.options = [
-        {key:"Minuto", value:"minute"},
-        {key:"Hora", value:"hour"},
-        {key:"Día", value:"day"},
-        {key:"Semana", value:"week"},
-        {key:"Mes", value:"month"},
-        {key:"Año", value:"year"}
-        ];
+    var granularity = 'day';
 
-    var histogram_enddate = new Date().toISOString().slice(0,10);
+    $scope.options = [
+        { key: "Minuto", value: "minute" },
+        { key: "Hora", value: "hour" },
+        { key: "Día", value: "day" },
+        { key: "Semana", value: "week" },
+        { key: "Mes", value: "month" },
+        { key: "Año", value: "year" }
+    ];
+
+    var histogram_enddate = new Date().toISOString().slice(0, 10);
 
     var histogram_startdate = new Date();
-        histogram_startdate.setMonth(histogram_startdate.getMonth() - 2);
-        histogram_startdate = histogram_startdate.toISOString().slice(0,10);
+    histogram_startdate.setMonth(histogram_startdate.getMonth() - 2);
+    histogram_startdate = histogram_startdate.toISOString().slice(0, 10);
 
 
-        $scope.windowsWidth = $window.innerWidth;
+    $scope.windowsWidth = $window.innerWidth;
 
-        $scope.selectedItem = function(selected){
-            granularity = selected;
-            var data = {startdate: histogram_startdate,
-                        enddate: histogram_enddate,
-                        countby: granularity};
+    $scope.selectedItem = function (selected) {
+        granularity = selected;
+        var data = {
+            startdate: histogram_startdate,
+            enddate: histogram_enddate,
+            countby: granularity
+        };
 
-            $http({
-                    method: 'POST',
-                    url: '/get_data/articles/histogram',
-                    data: $.param(data)
-                    }).then(function successCallback(response) {
-                        $("#histogram").empty();
-                        var histograma = generate_histogram(width=($scope.windowsWidth-300),height=300, data_json=response.data);
-                        console.log(histograma);
+        $http({
+            method: 'POST',
+            url: '/get_data/articles/histogram',
+            data: $.param(data)
 
-                    }, function errorCallback(response) {
-                        console.log(response);
-                    });
+        }).then(function successCallback(response) {
+            $("#histogram").empty();
+            var histograma = generate_histogram(width = ($scope.windowsWidth - 300), height = 300, data_json = response.data);
+            
+        }, function errorCallback(response) {
+            console.log(response);
+        });
 
     }
     //Draw Histogram for first time
     $scope.selectedItem(granularity);
 
+
+    //Section to control the TAG sistem
+    var should_contain = new Taggle('should', { placeholder: 'Concepto o frase importante en mi búsqueda' });
+    var must_contain = new Taggle('and', { placeholder: 'Concepto o frase fundamental en mi búsqueda' });
+    var not_contain = new Taggle('not', { placeholder: 'Concepto o frase que permite excluir resultados' });
+
+    function generate_array(data_array) {
+        var results = [];
+        if (data_array.length > 0) {
+            var match = "";
+            for (i = 0; i < data_array.length; i++) {
+                if (data_array[i].indexOf(" ") == -1) {
+                    match = match + data_array[i] + " ";
+                }
+                else {
+                    results.push({ "match_phrase": data_array[i] });
+                }
+            }
+            results.push({ "match": match });
+        }
+        else {
+            return results;
+        }
+
+        return results;
+    }
+
+    $scope.get_input_data = function() {
+
+        var should_contain_group = should_contain.getTagValues();
+        var must_contain_group = must_contain.getTagValues();
+        var not_contain_group = not_contain.getTagValues();
+
+        should_contain_group = generate_array(should_contain_group);
+        must_contain_group = generate_array(must_contain_group);
+        not_contain_group = generate_array(not_contain_group);
+
+        var json_data = {
+            "index": "articles",
+            "fields": ["art_title", "art_content"],
+            "and": must_contain_group,
+            "or": should_contain_group,
+            "not_and": not_contain_group,
+        }
+
+        /*
+        $.ajax({
+            url: '/get_data/articles/articles_advance_search',
+            type: 'POST',
+            data: {
+                json_data: JSON.stringify(json_data),
+                csrfmiddlewaretoken: '{{ csrf_token }}'
+            },
+            success: function (data) {
+                $('#article-list').remove();
+                $('#articles-container').append(data);
+            },
+            failure: function (data) {
+                alert('Error de conexión');
+            },
+            crossDomain: true
+        }); */
+
+         console.log(JSON.stringify(json_data));
+    }
 
 }]);
 
--- SophiaGUI/explora/static/explora/visual/histograma/histograma.js
+++ SophiaGUI/explora/static/explora/visual/histograma/histograma.js
@@ -1,29 +1,29 @@
-function generate_histogram(width, height, data_json){
+function generate_histogram(width, height, data_json) {
 
-  var parseDate = d3.time.format("%Y-%m-%d %H:%M:%S").parse
+    var parseDate = d3.time.format("%Y-%m-%d %H:%M:%S").parse
 
-  var margin = {top:20, right:40, bottom:50, left: 100};
-  var margin2 = {top:20, right:40, bottom:50, left:100};
+    var margin = { top: 20, right: 40, bottom: 50, left: 100 };
+    var margin2 = { top: 20, right: 40, bottom: 50, left: 100 };
 
-  var w = width - margin.left - margin.right;
-  var h = height - margin.top - margin.bottom;
-  var h2 = height/2 - margin2.top - margin2.bottom;
+    var w = width - margin.left - margin.right;
+    var h = height - margin.top - margin.bottom;
+    var h2 = height / 2 - margin2.top - margin2.bottom;
 
-  var chart = d3.select("#histogram")
-              .append("svg")
-              .attr("class","chart")
-              .attr("width",w + margin.left + margin.right)
-              .attr("height",h + margin.top + margin.bottom)
-              .append("g")
-              .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    var chart = d3.select("#histogram")
+        .append("svg")
+        .attr("class", "chart")
+        .attr("width", w + margin.left + margin.right)
+        .attr("height", h + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-   var minichart = d3.select("#histogram")
-              .append("svg")
-              .attr("class","chart")
-              .attr("width",w + margin2.left + margin2.right)
-              .attr("height",h2 + margin2.top + margin2.bottom)
-              .append("g")
-              .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
+    var minichart = d3.select("#histogram")
+        .append("svg")
+        .attr("class", "chart")
+        .attr("width", w + margin2.left + margin2.right)
+        .attr("height", h2 + margin2.top + margin2.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
 
     var x = d3.time.scale()
         .range([0, w]);
@@ -33,10 +33,10 @@ function generate_histogram(width, height, data_json){
         .range([h, 0]);
 
     var x2 = d3.time.scale()
-            .range([0, w]);
+        .range([0, w]);
 
     var y2 = d3.scale.linear()
-            .range([h2, 0]);
+        .range([h2, 0]);
 
 
     var xAxis = d3.svg.axis()
@@ -54,181 +54,182 @@ function generate_histogram(width, height, data_json){
         .orient("left")
 
     var xAxis2 = d3.svg.axis()
-                    .scale(x2)
-                    .tickSize(5, 1)
-                    .tickPadding(6)
-                    .orient("bottom");
+        .scale(x2)
+        .tickSize(5, 1)
+        .tickPadding(6)
+        .orient("bottom");
 
     var yAxis2 = d3.svg.axis()
-                    .scale(y2)
-                    .ticks(6)
-                    .tickSize(8, 1)
-                    .orient("left")
+        .scale(y2)
+        .ticks(6)
+        .tickSize(8, 1)
+        .orient("left")
 
     chart.append("g")
-                .attr("class", "y axis")
-                .call(yAxis);
+        .attr("class", "y axis")
+        .call(yAxis);
 
     chart.append("g")
-                .attr("class", "x axis")
-                .attr("transform", "translate(0," + h + ")")
-                .call(xAxis);
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + h + ")")
+        .call(xAxis);
 
     minichart.append("g")
-                .attr("class", "y axis")
-                .call(yAxis2);
+        .attr("class", "y axis")
+        .call(yAxis2);
 
     minichart.append("g")
-                .attr("class", "x axis")
-                .attr("transform", "translate(0," + h2 + ")")
-                .call(xAxis2);
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + h2 + ")")
+        .call(xAxis2);
 
     chart.append("text")
-         .attr("x",w/2)
-         .attr("y",h+40)
-         .style("text-anchor", "middle")
-         .text("Fecha");
+        .attr("x", w / 2)
+        .attr("y", h + 40)
+        .style("text-anchor", "middle")
+        .text("Fecha");
 
     chart.append("text")
-             .attr("x",-(margin.left - 25))
-             .attr("y",h/2)
-             .style("text-anchor", "middle")
-             .text("Cantidad");
+        .attr("x", -(margin.left - 25))
+        .attr("y", h / 2)
+        .style("text-anchor", "middle")
+        .text("Cantidad");
 
     var brush = d3.svg.brush()
-                     .x(x2)
-                     .on("brushend",brushed);
+        .x(x2)
+        .on("brushend", brushed);
 
-      //The input data is stored in data variable, this should change later...
-      data = data_json;
-      var m_e = data.length;
-      //console.log(m_e);
+    //The input data is stored in data variable, this should change later...
+    data = data_json;
+    var m_e = data.length;
+    //console.log(m_e);
 
-      data.forEach(function(d) {
-            d.key_as_string = d.key_as_string.substring(0, 19);
-            d.key_as_string = parseDate(d.key_as_string);
-            d.doc_count = +d.doc_count;
+    data.forEach(function (d) {
+        d.key_as_string = d.key_as_string.substring(0, 19);
+        d.key_as_string = parseDate(d.key_as_string);
+        d.doc_count = +d.doc_count;
+    });
+
+    x.domain([d3.min(data, function (d) { return d.key_as_string; }),
+    d3.max(data, function (d) { return d.key_as_string; })]);
+
+    y.domain([0, d3.max(data, function (d) { return d.doc_count; })]);
+
+
+    x2_min = d3.min(data, function (d) { return d.key_as_string; });
+    x2_max = d3.max(data, function (d) { return d.key_as_string; });
+
+    x2.domain([x2_min, x2_max]);
+
+    y2.domain([0, d3.max(data, function (d) { return d.doc_count; })]);
+
+    chart.select("g.y.axis").call(yAxis);
+    chart.select("g.x.axis").call(xAxis);
+
+
+    minichart.append("g")
+        .attr("class", "brush")
+        .call(brush)
+        .selectAll('rect')
+        .attr("height", h2);
+
+    minichart.selectAll(".bar")
+        .data(data)
+        .enter()
+        .append("rect")
+        .filter(function (d) {
+            return d.key_as_string >= x2_min && d.key_as_string <= x2_max;
+        })
+        .attr("class", "bar")
+        .attr("x", function (d) { return x2(d.key_as_string); })
+        .attr("width", function (d) {
+            return w / (m_e - 1);
+        })
+        .attr("y", function (d) { return y2(d.doc_count); })
+        .attr("height", function (d) { return h2 - y2(d.doc_count); })
+        .style("fill", "#078770");
+
+    minichart.select("g.y.axis").call(yAxis2);
+    minichart.select("g.x.axis").call(xAxis2);
+
+    minichart.append("g")
+        .attr("class", "brush")
+        .call(brush)
+        .selectAll('rect')
+        .attr("height", h2);
+
+
+    function brushed() {
+
+        var brush_values = brush.extent();
+
+        //Cambiar el dominio para que sean solo dentro de los valores del selected data
+        //x.domain([brush_values[0],brush_values[1]]);
+
+        selected_data = data.filter(function (d) {
+            return d.key_as_string >= brush_values[0] && d.key_as_string <= brush_values[1];
         });
 
-      x.domain([d3.min(data, function(d){ return d.key_as_string;}),
-                d3.max(data, function(d){ return d.key_as_string;})]);
-
-      y.domain([0, d3.max(data, function(d){ return d.doc_count;})]);
+        var n_e = selected_data.length;
 
 
-      x2_min = d3.min(data, function(d){ return d.key_as_string;});
-      x2_max = d3.max(data, function(d){ return d.key_as_string;});
 
-      x2.domain([x2_min,x2_max]);
-
-      y2.domain([0, d3.max(data, function(d){ return d.doc_count;})]);
-
-      chart.select("g.y.axis").call(yAxis);
-      chart.select("g.x.axis").call(xAxis);
+        x.domain([d3.min(selected_data, function (d) { return d.key_as_string; }),
+        d3.max(selected_data, function (d) { return d.key_as_string; })]);
 
 
-      minichart.append("g")
-               .attr("class","brush")
-               .call(brush)
-               .selectAll('rect')
-               .attr("height",h2);
+        chart.selectAll(".bar2").remove();
 
-      minichart.selectAll(".bar")
-              .data(data)
-              .enter()
-              .append("rect")
-              .filter(function(d){
-                 return d.key_as_string >= x2_min && d.key_as_string <= x2_max;
-              })
-              .attr("class", "bar")
-              .attr("x", function(d) { return x2(d.key_as_string); })
-              .attr("width", function(d){
-                         return w/(m_e-1);
-                    })
-               .attr("y", function(d) { return y2(d.doc_count); })
-               .attr("height", function(d) { return h2 - y2(d.doc_count); })
-               .style("fill","#078770");
+        var ymax = 0;
+        chart.selectAll(".bar2")
+            .data(data)
+            .enter()
+            .append("rect")
+            .filter(function (d) {
+                return d.key_as_string >= brush_values[0] && d.key_as_string <= brush_values[1];
+            })
+            .each(function (d) {
+                if (d.doc_count > ymax) {
+                    ymax = d.doc_count;
+                    y.domain([0, ymax]);
+                }
+            })
+            .attr("class", "bar2")
+            .attr("x", function (d) {
+                return x(d.key_as_string);
+            })
+            .attr("width", function (d) {
+                e = Object.keys(d).length;
+                return w / (n_e - 1);
+            })
+            .attr("y", function (d) { return y(d.doc_count); })
+            .attr("height", function (d) { return h - y(d.doc_count); })
+            .on("mouseover", function (d) {
 
-      minichart.select("g.y.axis").call(yAxis2);
-      minichart.select("g.x.axis").call(xAxis2);
-
-      minichart.append("g")
-               .attr("class","brush")
-               .call(brush)
-               .selectAll('rect')
-               .attr("height",h2);
-
-
-      function brushed(){
-
-          var brush_values = brush.extent();
-
-          //Cambiar el dominio para que sean solo dentro de los valores del selected data
-          //x.domain([brush_values[0],brush_values[1]]);
-
-          selected_data = data.filter(function(d){
-             return d.key_as_string >= brush_values[0] && d.key_as_string <= brush_values[1];
-          });
-
-          var n_e = selected_data.length;
-
-        
-
-          x.domain([d3.min(selected_data, function(d){ return d.key_as_string;}),
-                    d3.max(selected_data, function(d){ return d.key_as_string;})]);
+                cord = d3.mouse(this);
+                d3.select(this).style("fill", "#66cdaa");
+                d3.select("#tip")
+                    .style("left", cord[0] - 100 + "px")
+                    .style("top", cord[1] - 100 + "px")
+                    .style("opacity", 1);
+                d3.select("#value").text(d.doc_count);
+                d3.select("#tip").select("#date").text(d.key_as_string);
+            })
+            .on("mouseout", function (d) {
+                d3.select(this).style("fill", "#078770");
+                d3.select("#tip")
+                    .style("left", 0 + "px")
+                    .style("top", 0 + "px")
+                    .style("opacity", 0);
+            })
+            .style("fill", "#078770");
 
 
-          chart.selectAll(".bar2").remove();
+        chart.select("g.y.axis").call(yAxis);
+        chart.select("g.x.axis").call(xAxis);
 
-          var ymax = 0;
-          chart.selectAll(".bar2")
-                  .data(data)
-                  .enter()
-                  .append("rect")
-                  .filter(function(d){
-                     return d.key_as_string >= brush_values[0] && d.key_as_string <= brush_values[1];
-                  })
-                  .each(function(d){
-                    if (d.doc_count > ymax){
-                      ymax = d.doc_count;
-                      y.domain([0,ymax]);
-                    }
-                  })
-                  .attr("class", "bar2")
-                  .attr("x", function(d) {
-                      return x(d.key_as_string); })
-                  .attr("width", function(d){
-                      e = Object.keys(d).length;
-                      return w/(n_e - 1);
-                  })
-                  .attr("y", function(d) { return y(d.doc_count); })
-                  .attr("height", function(d) { return h - y(d.doc_count); })
-                  .on("mouseover",function(d){
-
-                   cord = d3.mouse(this);
-                   d3.select(this).style("fill","#66cdaa");
-                   d3.select("#tip")
-                        .style("left",cord[0] - 100  + "px")
-                        .style("top", cord[1] - 100 + "px")
-                        .style("opacity", 1);
-                    d3.select("#value").text(d.doc_count);
-                    d3.select("#tip").select("#date").text(d.key_as_string);
-                  })
-                  .on("mouseout",function(d){
-                     d3.select(this).style("fill","#078770");
-                     d3.select("#tip")
-                     .style("left", 0 + "px")
-                     .style("top", 0 + "px")
-                     .style("opacity", 0);
-                    })
-                  .style("fill","#078770");
-
-
-          chart.select("g.y.axis").call(yAxis);
-          chart.select("g.x.axis").call(xAxis);
-
-          //brush brush_values converted into date format yyyy-MM-dd
-          var startdate = brush_values[0].toISOString().slice(0,10);
-          var enddate = brush_values[1].toISOString().slice(0,10);
+        //brush brush_values converted into date format yyyy-MM-dd
+        var startdate = brush_values[0].toISOString().slice(0, 10);
+        var enddate = brush_values[1].toISOString().slice(0, 10);
     }
 }
