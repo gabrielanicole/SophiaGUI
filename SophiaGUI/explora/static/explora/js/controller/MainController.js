bfb573b1ce19184cf3572c49a29b64bcdced09c1
maravenag <aravenagamboa@gmail.com>
2017-01-16 16:32:38 -0300

    add datepicker for news cases search

--- SophiaGUI/explora/static/explora/js/controller/MainController.js
+++ SophiaGUI/explora/static/explora/js/controller/MainController.js
@@ -401,6 +401,27 @@ app.controller('newsCasesController', ['$scope', '$http', 'dataFormat', function
     $scope.size = 3;
     $scope.news_cases;
 
+    //Date Picker Setup
+    $("#datepicker1").datepicker({
+        format: "yyyy-mm-dd",
+        language: "es",
+        orientation: "bottom auto",
+    });
+
+    $("#datepicker2").datepicker({
+        format: "yyyy-mm-dd",
+        language: "es",
+        orientation: "bottom auto",
+    });
+
+    var today = new Date().toISOString().slice(0, 10);
+    $("#datepicker1").datepicker('update', String(today));
+    $("#datepicker2").datepicker('update', String(today));
+
+    var should_contain = new Taggle('should', { placeholder: 'Concepto o frase importante en mi búsqueda' });
+    var must_contain = new Taggle('and', { placeholder: 'Concepto o frase fundamental en mi búsqueda' });
+    var not_contain = new Taggle('not', { placeholder: 'Concepto o frase que permite excluir resultados' });
+
     $scope.range = function (min, max) {
         var output = [];
         for (var i = min; i <= max; i++) {
@@ -411,6 +432,16 @@ app.controller('newsCasesController', ['$scope', '$http', 'dataFormat', function
 
     $scope.page_number = function (page_number) {
         //Function to get the page number from view
+        var tag_values = dataFormat.get_tag_values(should_contain, must_contain, not_contain);
+        query = {
+            "and": tag_values.must_contain_group,
+            "or": tag_values.should_contain_group,
+            "not_and": tag_values.not_contain_group,
+            "startdate": $("#datepicker1").datepicker('getDate').toISOString().slice(0,10),
+            "endate": $("#datepicker2").datepicker('getDate').toISOString().slice(0,10),
+        }
+        console.log(JSON.stringify(query));
+
         var page = page_number;
         $http({
             method: 'GET',
@@ -427,15 +458,11 @@ app.controller('newsCasesController', ['$scope', '$http', 'dataFormat', function
                 var range = dataFormat.get_pagination_range($scope.actual_page, $scope.size, $scope.totalpages);
                 $scope.page_init = range.page_init;
                 $scope.page_end = range.page_end;
-
             }
         }, function errorCallback(response) {
             return (response);
         });
     }
     $scope.page_number(1);
-
-
-
 
 }]);
