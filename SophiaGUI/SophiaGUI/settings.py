62f6d537d6026c6404dbeece66a8195b112cc330
maravenag <aravenagamboa@gmail.com>
2017-01-31 17:39:34 -0300

    fix settings.py

--- SophiaGUI/SophiaGUI/settings.py
+++ SophiaGUI/SophiaGUI/settings.py
@@ -169,19 +169,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-#STATIC_URL = '/static/'
-
-#STATIC_URL = '/static/'
 #BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-#STATICFILES_DIRS = (
-                #    os.path.join(BASE_DIR, STATIC_URL),
-             #   )
 
-#PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-#STATIC_ROOT = os.path.join(PROJECT_ROOT, '/explora/static/')
 
 STATIC_URL = '/static/'
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_DIR=os.path.dirname(__file__)
-STATIC_ROOT = os.path.join(PROJECT_DIR, '../explora/static/')
+STATIC_ROOT = os.path.join(PROJECT_DIR, '/explora/static/')
+STATICFILES_DIRS = (
+                    os.path.join(BASE_DIR, STATIC_URL),
+                )
+#STATIC_ROOT = os.path.join(PROJECT_DIR, '../explora/static/')
