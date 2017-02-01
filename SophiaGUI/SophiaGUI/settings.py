504f4ff78eacd2cb345bb12ad6758f1f5088c4db
maravenag <aravenagamboa@gmail.com>
2017-02-01 09:17:56 -0300

    changes in settings.py

--- SophiaGUI/SophiaGUI/settings.py
+++ SophiaGUI/SophiaGUI/settings.py
@@ -166,22 +166,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-
-#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-#STATIC_URL = '/static/'
-#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-#PROJECT_DIR=os.path.dirname(__file__)
-#STATIC_ROOT = os.path.join(PROJECT_DIR, '/explora/static/')
-#STATICFILES_DIRS = (
-#                    os.path.join(PROJECT_DIR, '../explora/static/'),
-#                )
-#STATIC_ROOT = os.path.join(PROJECT_DIR, '../explora/static/')
-
 STATIC_URL = '/static/'
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_DIR=os.path.dirname(__file__)
 STATIC_ROOT = os.path.join(PROJECT_DIR, '../explora/static/')
