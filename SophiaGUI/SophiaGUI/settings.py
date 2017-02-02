813eecb172ac5312bef75bedbd759ba3ac01f2ac
Javier Pérez Ferrada <javierperezferrada@gmail.com>
2017-02-02 16:39:50 -0300

    send mail added

--- SophiaGUI/SophiaGUI/settings.py
+++ SophiaGUI/SophiaGUI/settings.py
@@ -177,3 +177,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 STATICFILES_DIRS = (
                     os.path.join(BASE_DIR, 'explora/static/'),
                 )
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'sophiaproject4@gmail.com'
+EMAIL_HOST_PASSWORD = 'test1test'
+EMAIL_PORT = 587
