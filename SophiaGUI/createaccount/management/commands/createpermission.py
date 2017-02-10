de36359568e1775dc2e4dcb0277031885964db50
maravenag <aravenagamboa@gmail.com>
2017-02-10 16:35:55 -0300

    fix createpermission

--- SophiaGUI/createaccount/management/commands/createpermission.py
+++ SophiaGUI/createaccount/management/commands/createpermission.py
@@ -9,10 +9,14 @@ class Command(BaseCommand):
         pass
 
     def handle(self, *args, **options):
+        try:
             group, created = Group.objects.get_or_create(name='Analistas')
             content_type = ContentType.objects.get(app_label='explora', model='analist')
             permission = Permission.objects.create(codename='isAnalist',
                                                    name='Is Analist',
-                                                   content_type=content_type)
+                                                    content_type=content_type)
             group = Group.objects.get(name='Analistas')
             group.permissions.add(permission)
+            print "Group and permissions created successfully"
+        except Exception as e:
+            print e
